9babcb4ab64a0b97a88a3644679c78fd43c8f3a8
Kshitij Bhat <kb@programax.org>
2020-02-09 13:23:49 -0500

    Fixing the issues with the original script

--- Others/downloadData.sh
+++ Others/downloadData.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -d "../Data" ]; then
-	mkdir ../Data
+if [ ! -d "../data" ]; then
+	mkdir ../data
 fi
 
 
-wget -bqco download_log.log "https://onedrive.live.com/?authkey=%21AFYQkl1tP%2DQh3Ek&id=2B888FC2F8F47809%21857&cid=2B888FC2F8F47809#authkey=%21AFYQkl1tP%2DQh3Ek&cid=2B888FC2F8F47809&id=2B888FC2F8F47809%21858&parId=2B888FC2F8F47809%21857&action=defaultclick" -O ../Data/SmokeData.zip
+wget -bqco download_log.log "https://onedrive.live.com/?authkey=%21AFYQkl1tP%2DQh3Ek&id=2B888FC2F8F47809%21857&cid=2B888FC2F8F47809#authkey=%21AFYQkl1tP%2DQh3Ek&cid=2B888FC2F8F47809&id=2B888FC2F8F47809%21858&parId=2B888FC2F8F47809%21857&action=defaultclick" -O ../data/SmokeData.zip
 
 
 echo "Download is proceeding in the background."
@@ -13,13 +13,14 @@ echo "Should a problem occur, rerunning this script, will resume the download."
 echo "wget output is being directed to download_log.log"
 echo "use 'tail -f download_log.log' to check progress"
 
+cd ../data/
 unzip SmokeData.zip
 
 if [ $? -eq 0 ]
 then
     echo "The files are sucessfully unzipped"
     echo "The directory has below folders:"
-    ls -1 Data/SmokeData
+    ls -1 SmokeData/
 else
     echo "error unzipping files"
 fi
