ba03856937119a4228b8b4e6e78f901e5e631518
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2020-03-26 11:40:53 -0400

    Add a utility function to create datasets
    And some documentation improvements.

--- Models/SmokeDataset.py
+++ Models/SmokeDataset.py
@@ -1,6 +1,9 @@
 import torch
+import numpy as np
+import pandas as pd
 
 from PIL import Image
+from sklearn.model_selection import train_test_split
 from torchvision import transforms as tf
 from torch.utils.data import Dataset
 
@@ -9,9 +12,11 @@ class SmokeDataset(Dataset):
     """
     Extend PyTorch's Dataset for our specific purposes.
     """
-    def __init__(self, path_df, image_ids, train=True):
+    def __init__(self, path_df: pd.DataFrame, image_ids: list, train=True):
         """
         Load the images corresponding to `image_ids` as PIL's Image objects and the corresponding labels as a torch tensor.
+        Image paths are read from the `path_df` `DataFrame`.
+        Value of `train` determined which transforms to apply to the images.
         """
         if train:
             downsize = 230
@@ -33,7 +38,6 @@ class SmokeDataset(Dataset):
 
 def get_transforms(train=True):
     pil_to_tensor = tf.ToTensor()
-    tensor_to_pil = tf.ToPILImage()
     crop = tf.RandomCrop((224, 224))
     hflip = tf.RandomHorizontalFlip()
     vflip = tf.RandomVerticalFlip()
@@ -43,3 +47,28 @@ def get_transforms(train=True):
     else:
         transforms = tf.Compose([pil_to_tensor, normalize])
     return transforms
+
+
+def get_datasets():
+    image_path_df = pd.read_csv("Image_Paths.csv")
+    label_encoding = {"Cloud": 0, "Dust": 1, "Haze": 2, "Land": 3, "Seaside": 4, "Smoke": 5}
+    image_path_df.image_type.replace(label_encoding, inplace=True)
+
+    class_weights = len(image_path_df) / image_path_df.image_type.value_counts().values / 6
+    # 20% of data is testing data, get its indices
+    train_valid_indices, test_indices = train_test_split(
+        np.arange(len(image_path_df)), test_size=0.2,
+        stratify=image_path_df.image_type
+    )
+    # 16% of the original data is validation data, which comes out to be 20% of combined training and validation data
+    # Again, get its and training set's indices
+    train_indices, valid_indices = train_test_split(
+        train_valid_indices, test_size=0.2,
+        stratify=image_path_df.image_type[train_valid_indices]
+    )
+
+    train_data = SmokeDataset(image_path_df, train_indices, train=True)
+    validation_data = SmokeDataset(image_path_df, valid_indices, train=False)
+    test_data = SmokeDataset(image_path_df, test_indices, train=False)
+
+    return class_weights, train_data, validation_data, test_data
